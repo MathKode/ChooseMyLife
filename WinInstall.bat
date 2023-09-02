4a34f5cdea555a8adafec7897381277428b629a0
BiMathAx <61063210+MathKode@users.noreply.github.com>
2023-09-02 23:22:40 +0200

    Create WinInstall.bat

---
+++ WinInstall.bat
@@ -0,0 +1,10 @@
+@echo off
+Windows\python.exe -m venv .venv/ChooseMyLife
+.venv\ChooseMyLife\Scripts\activate.bat
+pip install django
+
+set /P id=Do you want start service at every start-up [Y/N] : 
+IF %id%==Y (
+  copy "Windows\start_server.bat"  "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start_server.bat"
+)
+
